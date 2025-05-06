a587bf1e55189a7e3622a3c386990316856b39de
Piotr Słupski <piotroxp@gmail.com>
2025-05-06 13:55:26 +0200

    Variable name

--- server/index.js
+++ server/index.js
@@ -155,7 +155,7 @@ app.post('/api/heroes/generate/:id', authMiddleware, async (req, res) => {
   }
 
   // Check if the user owns this hero
-  if (hero.userId !== userId) {
+  if (hero.userid !== userId) {
     return res.status(403).json({ error: 'You do not have permission to modify this hero' });
   }
 
